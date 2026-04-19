d53e65ac6587d52b66b4dff713d22cc1f5ac8906
Alex Vincent <alex@far2cool.com>
2026-04-19 13:53:03 +0200

    fixup devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,11 +4,6 @@ echo "postCreateCommand.sh"
 echo "--------------------"
 
 sudo apt-get update
-
-# echo "Setup pre-commit"
-#pre-commit
-#pre-commit install --install-hooks
-#pre-commit run --all-files --verbose
-#pre-commit autoupdate
+sudo apt-get upgrade -y
 
 sudo chmod +x .devcontainer/postStartCommand.sh
